0566e3c0c94932ab3a74e0df6254842c27036fba
Crowdbotics <team@crowdbotics.com>
2020-10-24 19:00:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fallout_shelter_2_21915/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fallout_shelter_2_21915;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "fallout_shelter_2_21915";
+    }
+}
---
+++ ios/fallout_shelter_2_21915Tests/fallout_shelter_2_21915Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface fallout_shelter_2_21915 : XCTestCase
+
+@end
+
+@implementation fallout_shelter_2_21915
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
